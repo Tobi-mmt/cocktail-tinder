0e3468e43ca4bc90b4b342abd2ebe4d6f6027e92
Tobias <tobi.l@posteo.at>
2019-09-09 15:03:27 +0200

    chore: Init app

---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <md-card>
+    <md-card-media>
+      <img src="https://www.thecocktaildb.com/images/media/drink/ruxuvp1472669600.jpg" alt="People">
+    </md-card-media>
+
+    <md-card-header>
+      <div class="md-title">Title goes here</div>
+      <div class="md-subhead">Subtitle here</div>
+    </md-card-header>
+
+    <md-card-expand>
+      <md-card-actions md-alignment="space-between">
+        <div>
+          <md-button>Action</md-button>
+        </div>
+
+        <md-card-expand-trigger>
+          <md-button>Learn more</md-button>
+        </md-card-expand-trigger>
+      </md-card-actions>
+
+      <md-card-expand-content>
+        <md-card-content>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+        </md-card-content>
+      </md-card-expand-content>
+    </md-card-expand>
+  </md-card>
+</template>
+
+
+
+<script>
+  export default {
+    name: 'Card'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .card-expansion {
+    height: 480px;
+  }
+
+  .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
